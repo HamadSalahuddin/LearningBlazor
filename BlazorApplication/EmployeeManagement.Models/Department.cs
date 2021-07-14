91820ba557ac471c96ce3bf516cf653a8c15b6e5
HamadSalahuddin <hamadsalahuddin@gmail.com>
2021-07-14 16:33:21 +0500

    Added Model class library

---
+++ BlazorApplication/EmployeeManagement.Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public int DepartmentName { get; set; }
+
+    }
+}
